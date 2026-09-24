e406745a77bd6d638c8ca7f8a5e1a5680686245b
Matteo Baccanelli <m.baccanelli1@studenti.unibg.it>
2026-09-24 17:24:23 +0200

    Update App.java

--- codice/calculator/src/main/java/se/unibg/it/calculator/App.java
+++ codice/calculator/src/main/java/se/unibg/it/calculator/App.java
@@ -7,6 +7,9 @@ public class App {
 	private static Logger logger = LogManager.getLogger(App.class);
 
 	public static void main(String[] args) {
-		logger.error("Error log message");
+		logger.error("Somma di 3 e 5: " + Calculator.Somma(3, 5));
+		logger.error("Differenza di 8 e 2: " + Calculator.Differenza(8, 2));
+		logger.error("Prodotto di 4 e -2: " + Calculator.prod(4, -2));
+		logger.error("Divisione di 6 e 3: " + Calculator.div(6, 3));
 	}
 }
